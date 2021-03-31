b8fb275c8e065eb6d637def4a5fd3995270837e0
Ben Russell <benjaminrussell36@gmail.com>
2021-03-31 11:01:41 -0700

    update my description in the about section

--- src/components/LandingPage/About.js
+++ src/components/LandingPage/About.js
@@ -11,12 +11,12 @@ export default function About() {
         <h3 className='about-title'>About Me</h3>
         <div className='about-content'>
           <Image className='about-img' src={benrussell} alt='Portrait of Benjamin Russell' />
-          <p className='about-text'>I am a 34 year old living in Portland, Oregon and before I decided to change careers I was a carpenter/handyman.
-            I have also worked in the printing industry before. I made the change to web development after my father-in-law retired and I did not want to continue running the business by myself.
-            I still do some carpentry as a hobby and am creating small tables and desks for my family and friends in my free time.
-            While attending Epicodus I discovered that I enjoy working on the front end of web sites primarily. React was some of the most fun I had while studying there.
-            That being said we did learn about back end technologies and I also enjoy making a site function as smoothly and realiably as possible for all users.
-            I am now searching for a job where I can utilize my strengths in front end development and sharpen my skills when it comes to back end, for example authentication and database management.
+          <p className='about-text'>I am a 34 year old living in Portland, Oregon and web development was my hobby and part-time job while I was running a full-time construction company.
+            I have also worked in the printing industry before. I moved to full-time web development after Covid-19 started in March 2020.
+            I still do some custom carpentry design as a hobby and am creating small tables and desks for my family and friends in my free time.
+            While working on design projects I learned that I enjoy working on the front end of web sites primarily. React was some of the most fun I have had.
+            That being said I did learn about back end technologies and I also enjoy making a site function as smoothly and realiably as possible for all users.
+            I am now searching for projects and jobs where I can utilize my strengths in front end development and sharpen my skills when it comes to back end, for example authentication and database management.
             This career path is facinating because there is so much you can do and so much you can learn every day.
           </p>
         </div>
