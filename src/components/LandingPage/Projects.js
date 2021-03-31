b1f04b6317362d93f4f1d14216ee8c302e387e58
Ben Russell <benjaminrussell36@gmail.com>
2021-03-31 11:21:14 -0700

    update descriptions of projects

--- src/components/LandingPage/Projects.js
+++ src/components/LandingPage/Projects.js
@@ -52,7 +52,7 @@ export default function Projects() {
                 <Image className='modal-img' src={gardenhelperhome} alt='Garden Helper Homepage' />
                 <div className='about-project'>
                   <p>
-                    This project was conceived as my capstone project to display the skills I learned while attending the Epicodus full stack web development course.
+                    This was a really fun project, as I built the frontend and the backend.
                     I used a React.js frontend with Google Firebase to handle my backend.<br/>
                     The application is designed to help new gardeners decide on some plants that will fit the conditions existing in their garden. 
                     I chose all the plants as ones that I have found are low-maintinence and most are low-water as well.
@@ -85,12 +85,12 @@ export default function Projects() {
               <Modal.Body>
                 <Image className='modal-img' src={damerowhome} alt='Damerow Ford Homepage' />
                 <div className='about-project'>
-                  <p>While interning at Theorem Agency, we were tasked with redesigning the homepage of this dealership.<br/> 
-                    We worked with the inhouse UI/UX designer on what the new page would look like. 
-                    The two of us changed the hero image and created the 2 buttons the are on top of the image.<br/> 
-                    We also redesigned the FAQ section to use an accordion when a user clicked on one of the questions. 
-                    We also worked on a gallery of images that would zoom in when a user hovered over them.<br/> 
-                    There was quite a bit of CSS work involved in all of the aspects we changed.
+                  <p>While working at Theorem Agency, I was tasked with redesigning the homepage of this dealership.<br/> 
+                    I worked with the inhouse UI/UX designer on what the new page would look like. 
+                    I changed the hero image and created the 2 buttons the are on top of the image.<br/> 
+                    I also redesigned the FAQ section to use an accordion when a user clicked on one of the questions. 
+                    I also worked on an interactive gallery of images that would zoom in when a user hovered over them.<br/> 
+                    There was CSS heavy work involved in all of the aspects we changed.
                     Here is a link to the live page <a href='https://www.damerowford.com/2021-ford-bronco.html' target='_blank' rel='noreferrer'>Damerow Ford.</a></p>
                 </div>
               </Modal.Body>
@@ -110,7 +110,7 @@ export default function Projects() {
               <Modal.Body>
                 <Image className='modal-img' src={mopopmodal} alt='MoPOP Board of Directors page' />
                 <div className='about-project'>
-                  <p>This project for the MoPOP Board of Directors was created while interning at Theorem Agency. 
+                  <p>This project for the MoPOP Board of Directors was created while working at Theorem Agency. 
                     I worked closely with the UI/UX designer and built the first mockup to their specifications. 
                     When it became clear that the initial version was not working quite the way we wanted it to function, we decided to try a couple new ideas.
                     I pitched an idea to have the cards, when clicked, drop down a description of the person on the board the user had clicked on.
@@ -130,16 +130,16 @@ export default function Projects() {
             <Image onClick={handleShow4} src={blazers} alt='Blazers pregame page cropped' />
             <Modal show={showModal4} onHide={handleClose4} id='mopop-modal' dialogClassName='modal-90w'>
               <Modal.Header closeButton>
-                <Modal.Title>Blazers News Items</Modal.Title>
+                <Modal.Title>TrailBlazers NBA Team News Items</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 <Image className='modal-img' src={blazersfull} alt='Blazers pregame page' />
                 <div className='about-project'>
-                  <p>I am a native Portlander and I was thrilled to learn that we would be working on the Trailblazers page while at Theorem Agency.
-                    This was the first project I was assigned to work on while there. The site uses a React frontend and we had just completed the React portion of our code school education.
-                    It was great to solidify those skills by working on redesigning the news feed portion of the blazers page. I had to pull the data from an API that the blazers made available to us and display it on the page.
-                    I used bootstrap cards and CSS flexbox to get the look the team wanted for their new news feed. My fellow interns also worked on other portions of this same page.
-                    We collaborated to make sure that everything we were doing would fit in well with each other.<br/>
+                  <p>I am a native Portlander and I was thrilled to work on the Trailblazers page while at Theorem Agency.
+                    The site uses a React frontend and redesigning the news feed portion of the Blazers website was the most interesting part.
+                    I had to pull the data from an API that the blazers made available to us and display it on the page.
+                    I used bootstrap cards and CSS flexbox to get the look the team wanted for their new news feed.
+                    There were a lot of stakeholders involved and I loved the collaboration on various elements to make sure that everything we were doing would align with the brand and spirit of the team.<br/>
                     Go Blazers!
                   </p>
                 </div>
@@ -160,9 +160,7 @@ export default function Projects() {
               <Modal.Body>
                 <Image className='modal-img' src={tindogfull} alt='Tindog home page' />
                 <div className='about-project'>
-                  <p>While deciding if I wanted to invest in my education and attend a code school, I took a class from UDemy.com.
-                    I made this page while working on that class. This project helped me learn how to properly use HTML, CSS, and Bootstrap.
-                    There was still much to learn, but this class helped me decide to go on to code school and round out my education.
+                  <p>This was something that started out as a fun discussion amongst friends, who love pets as much as I do, but I mostly use this site for testing new CSS I find interesting.
                     Here is a link to the live page <a href='https://brussell36.github.io/tindog/' target='_blank' rel='noreferrer'>Tindog.</a>
                   </p>
                 </div>
@@ -183,7 +181,7 @@ export default function Projects() {
               <Modal.Body>
                 <Image className='modal-img' src={theorem} alt='Theorem employee portal' />
                 <div className='about-project'>
-                  <p>During my internship at Theorem Agency they had an employee portal that had been designed, but had almost no styling.
+                  <p>While working at Theorem Agency they had an employee portal that had been designed, but had almost no styling.
                     I was tasked with making the pages look like the UI/UX designer was envisioning they would look like.
                     They all look like dashboards with quicklinks on the left hand side of the page. The data is not dynamic yet as they had not created the API I needed to utilize to get the proper information to show.
                     This project used React. Bootstrap was used heavily for the styles as well as SCSS for all of the custom styles.
